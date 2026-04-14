421f49342c64eb55b8c069ad95098831ad5bdf44
sofiastolz330-ctrl <sofia.stolz330@gmail.com>
2026-04-14 09:04:28 -0700

    main commit 4/14

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('magicBtn');
+
+button.addEventListener('click', () => {
+    alert("It works! You just deployed a site from the cloud!");
+    document.body.style.background = "linear-gradient(135deg, #ff9a9e 0%, #fecfef 99%)";
+}); 
