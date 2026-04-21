a01fc06ed75c025f59aea0d50cad89159b5eed87
sofiastolz330-ctrl <sofia.stolz330@gmail.com>
2026-04-20 18:56:24 -0700

    commit 4/20

--- projects/rocket-car.js
+++ projects/rocket-car.js
@@ -1,42 +1,79 @@
-// ── Rocket Car ────────────────────────────
-// Original sketch by jbologna-fuzzy
-var boxlen = 150;
-var boxheight = 50;
-var rndX = 1;
-var rndY = 1;
-var ahead = 1;
-var big = 1;
-var grn = 50;
+
+var X = 100
+let centerY = 200
+let size = 150;
+var Cmove = 2;
+
 function setup() {
-  let canvas = createCanvas(500, 500);
-  canvas.parent('sketch-container');
-  rndX = random(100, 300);
-  rndY = random(100, 300);
+  createCanvas(400, 400);
 }
+
+
 function draw() {
-  background(0, 150, 200);
-  fill(200, grn, 50);
-  rect(rndX, rndY, boxlen + big * 2, boxheight + big * 2);
-  fill(0, grn, 0);
-  triangle(
-    rndX + boxlen + big * 2,
-    rndY,
-    rndX + boxlen + boxlen / 2,
-    rndY + boxheight / 2,
-    rndX + boxlen + big * 2,
-    rndY + boxheight + big * 2
-  );
-  fill(50, 50, 50);
-  circle(rndX, rndY + boxheight, boxheight + big * 2);
-  circle(rndX + boxlen, rndY + boxheight, boxheight + big * 2);
-  rndX = rndX + ahead;
-  grn = grn + ahead * 2;
-  if (rndX + boxlen > width - 10) {
-    ahead = -1;
-    big = big - 5;
+  background(0,0,139);
+  
+  //stars
+  circle (10,100,5)
+  circle (50,100,5)
+  circle (136,100,5)
+  circle (356,100,5)
+  circle (280,100,5)
+  circle (40,150,5)
+  circle (99,150,5)
+  circle (230,150,5)
+  circle (354,150,5)
+  circle (289,150,5)
+  circle (29,50,5)
+  circle (157,50,5)
+  circle (127,50,5)
+  circle (253,50,5)
+  circle (333,50,5)
+  circle (253,200,5)
+  circle (23,200,5)
+  circle (105,200,5)
+  circle (222,200,5)
+  circle (270,250,5)
+  circle (70,250,5)
+  circle (160,250,5)
+  circle (370,250,5)
+  circle (300,300,5)
+  circle (40,300,5)
+  circle (137,300,5)
+  circle (200,300,5)
+  circle (27,350,5)
+  circle (134,350,5)
+  circle (397,350,5)
+  circle (306,350,5)
+  
+  if (X <= 50 || X >= width - 100) {
+    
   }
-  if (rndX < 10) {
-    ahead = 1;
-    big = big + 5;
+  if (centerY <= 50 || centerY >= height - 100) {
+    
   }
+
+  
+
+  
+  
+ //the car
+  fill("black");
+  rect(X - 100, centerY-100, 200, 80, 25);
+  fill("white")
+  ellipse(X + (size * 0.35), centerY - (size * 0.10), size * 0.5, size * 0.5);
+  ellipse(X - (size * 0.220), centerY - (size / 10), size * 0.5, size * 0.5);
+  X= X + Cmove
+  if (X> width-20) {
+    Cmove=-1;
+    
+  }
+ // if (X > width - 10) {
+ //   Cmove = 2;
+ // }
+  if(X < 20) {
+    Cmove = +20
+  }
+ 
+//stars
+  
 }
